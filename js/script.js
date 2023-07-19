62933ea2af8989bfbbb4c61084f5dad45a181d08
Pis4ni <pis4wrk@gmail.com>
2023-07-19 13:51:06 +0200

    add: first step (script js)

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* immagazzino il costo/km */
+const costKm = 0,21
+/* immagazzino il valore della maggiore età */
+const adult = 18
+/* immagazzino il valore dell' età pensionabile */
+const retairee = 65
+/* chiedo all' utente quanti chilometri ha intenzione di percorrere */
+
+/* chiedo all' utente la sua età */
+
+/* calcolo il prezzo esclusivamente sulla base della distanza/prezzo */
+
+/* SE l' età del richiedente è inferiore alla maggiore età */
+
+/* applico uno sconto del 20% sul prezzo totale del bilietto */
+
+/* ALTRIMENTI SE l' età è compresa tra la maggiore e l' età pensionabile non applico nessuno sconto */
+
+/* ALTRIMENTI applico uno sconto pari al 40% */
+
+/* fine. */
