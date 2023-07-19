9b4fbdce8836fbf557e298e9c4f9b090559df88b
Pis4ni <pis4wrk@gmail.com>
2023-07-19 17:58:34 +0200

    fix variables: name correction

--- js/script.js
+++ js/script.js
@@ -57,24 +57,24 @@ let lastPrice = initPrice
 let discountValue = 0
 
 /* immagazzino il valore di quale sconto verrà applicato*/
-let wichDiscount = 0
+let whichDiscount = 0
 
 /* SE l' età del richiedente è inferiore alla maggiore età */
 if (userAge < adult) {
     /* applico uno sconto del 20% sul prezzo totale del bilietto */
     discountValue = ( initPrice*discountMinor)/100
     lastPrice = initPrice - discountValue
-    wichDiscount = discountMinor
+    whichDiscount = discountMinor
     /* ALTRIMENTI SE l' età è compresa tra la maggiore e l' età pensionabile non applico nessuno sconto */
     /* ALTRIMENTI applico uno sconto pari al 40% */
 } else if (userAge>retairee) {
     discountValue = ( initPrice*discountMajor)/100
     lastPrice = initPrice - discountValue
-    wichDiscount = discountMajor
+    whichDiscount = discountMajor
 }
 /* fine. */
 console.log(lastPrice) 
 /* mostro a schermo il le varie operazioni */
-document.getElementById('display').innerHTML = "il prezzo iniziale del tuo biglietto è "+ initPrice + "€" + " al quale viene applicata la percentuale di sconto del " + wichDiscount + "%" + " il prezzo finale del tuo biglietto è " + lastPrice +"€" + " al quale è stato applicato uno sconto di: " + discountValue +"€"+ " Goditi il tuo viaggio!"
+document.getElementById('display').innerHTML = "il prezzo iniziale del tuo biglietto è "+ initPrice + "€" + " al quale viene applicata la percentuale di sconto del " + whichDiscount + "%" + " il prezzo finale del tuo biglietto è " + lastPrice +"€" + " al quale è stato applicato uno sconto di: " + discountValue +"€"+ " Goditi il tuo viaggio!"
 
 
